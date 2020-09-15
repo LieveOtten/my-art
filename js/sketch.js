c68753b017242bb7456d95a9d1293b30a0064948
LieveOtten <71327022+LieveOtten@users.noreply.github.com>
2020-09-15 15:07:17 +0200

    My p5 art toegevoegd

---
+++ js/sketch.js
@@ -0,0 +1,21 @@
+function setup (){
+
+createCanvas (200,200);
+background (100);
+print ("dit is de print versie");
+
+
+ellipseMode (CENTER);
+ellipse (100,100,200,200);
+rect (100,100,20,20);
+
+
+fill(0);
+textAlign(CENTER,CENTER);
+text("dit is het midden",100,100);
+}
+
+function draw () {
+
+
+}
